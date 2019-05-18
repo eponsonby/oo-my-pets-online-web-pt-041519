38bb06b27f54d2c5970bb3f9ef9c601454094ec4
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-18 16:15:09 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,19 @@
 class Cat
   attr_reader :name
-  attr_accessor :mood
   
   def initialize(name)
     @name = name
     @mood = "nervous"
+  end
+  
+  def mood= (mood)
+    if Owner.sell_pets
+      @mood = "nervous"
+    end
+  end
+  
+  def mood
+    @mood
   end
   
 end
